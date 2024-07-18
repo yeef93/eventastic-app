1241ec3886426152fd6a094d5c5b0265adc92853
yeef93 <yunindafaranika@gmail.com>
2024-07-18 13:56:23 +0700

    feat:fatch point, event and ticket data

--- app/users/[username]/dashboard/page.tsx
+++ app/users/[username]/dashboard/page.tsx
@@ -1,12 +1,79 @@
 "use client";
+
+import ProtectedLayout from "@/app/components/Users/ProtectedLayout";
 import {
   CalendarIcon,
   CurrencyDollarIcon,
   TicketIcon,
 } from "@heroicons/react/outline";
-import ProtectedLayout from "@/app/components/Users/ProtectedLayout";
+import React, { useState, useEffect } from "react";
+import { useSession } from "next-auth/react";
 
 function Dashboard() {
+  const apiUrl = process.env.NEXT_PUBLIC_API_BASE_URL;
+  const [points, setPoints] = useState(0);
+  const [totalTickets, setTotalTickets] = useState(0);
+  const [totalEvents, setTotalEvents] = useState(0);
+  const [error, setError] = useState<string | null>(null);
+  const { data: session } = useSession();
+
+  useEffect(() => {
+    const fetchPoints = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/users/me/points`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${session?.user?.token}`,
+          },
+        });
+        if (!response.ok) {
+          throw new Error("Failed to fetch points");
+        }
+        const data = await response.json();
+        setPoints(data.data.points);
+      } catch (err: any) {
+        console.error("Fetching points error:", err);
+        setError(err.message);
+      }
+    };
+
+    const fetchTickets = async () => {
+      try {
+        const response = await fetch(`${apiUrl}/users/me/tickets`, {
+          method: "GET",
+          headers: {
+            Authorization: `Bearer ${session?.user?.token}`,
+          },
+        });
+        if (!response.ok) {
+          throw new Error("Failed to fetch tickets");
+        }
+        const data = await response.json();
+        setTotalTickets(data.data.length);
+        const eventTitles = new Set(
+          data.data.map((ticket: any) => ticket.eventTitle)
+        );
+        setTotalEvents(eventTitles.size);
+      } catch (err: any) {
+        console.error("Fetching tickets error:", err);
+        setError(err.message);
+      }
+    };
+
+    fetchPoints();
+    fetchTickets();
+  }, [apiUrl]);
+
+  const formatPoints = (points: number) => {
+    return new Intl.NumberFormat("id-ID", { minimumFractionDigits: 2 }).format(
+      points
+    );
+  };
+
+  if (error) {
+    return <div>{error}</div>;
+  }
+
   return (
     <ProtectedLayout>
       <div>
@@ -17,7 +84,7 @@ function Dashboard() {
             </div>
             <div>
               <p className="mb-2 text-sm font-medium text-white">Points</p>
-              <p className="text-lg font-semibold text-white">Rp 10.000,00</p>
+              <p className="text-lg font-semibold text-white">Rp. {formatPoints(points)}</p>
             </div>
           </div>
           <div className="flex items-center p-4 bg-gradient-to-r from-fuchsia-600 to-purple-600 rounded-lg shadow-xs">
@@ -28,7 +95,7 @@ function Dashboard() {
               <p className="mb-2 text-sm font-medium text-white">
                 Total Events
               </p>
-              <p className="text-lg font-semibold text-white">5</p>
+              <p className="text-lg font-semibold text-white">{totalEvents}</p>
             </div>
           </div>
           <div className="flex items-center p-4 bg-gradient-to-r from-emerald-500 to-lime-600 rounded-lg shadow-xs">
@@ -39,7 +106,7 @@ function Dashboard() {
               <p className="mb-2 text-sm font-medium text-white">
                 Total Tickets
               </p>
-              <p className="text-lg font-semibold text-white">12</p>
+              <p className="text-lg font-semibold text-white">{totalTickets}</p>
             </div>
           </div>
         </div>
